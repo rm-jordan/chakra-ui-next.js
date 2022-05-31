72d07fd8e225f0aa730fb0fb16f816e843ecfcc5
rm-jordan <64985248+rm-jordan@users.noreply.github.com>
2022-05-31 09:47:01 -0400

    started page alignment

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,17 @@
-const IndexPage = () => <h1>Hello Next.js</h1>
+import { Container, Flex, VStack } from "@chakra-ui/react";
+
+const IndexPage = () => 
+  <Container maxW="container.xl" padding={0}>
+    <Flex h="100vh" py={20}>
+      <VStack w="full" h="full" p={10} spacing={10} alignItems="flex-start">
+      </VStack>
+      <VStack w="full" h="full" p={10} spacing={10} alignItems="flex-start" bg="gray.50">
+
+</VStack>
+    </Flex>
+  </Container>
 
 export default IndexPage;
+
+      //py = padding y axis
+      // bg = background "gray.50" <- design token from chakra UI
